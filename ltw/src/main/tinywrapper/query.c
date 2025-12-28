a123430780636ae58a0e3ecc17276fe51a517fbb
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-12-29 00:47:11 +0400

    Feat[query]: return stub result if timer_query not present

--- ltw/src/main/tinywrapper/query.c
+++ ltw/src/main/tinywrapper/query.c
@@ -7,15 +7,24 @@
 #include "egl.h"
 #include "proc.h"
 
-
 void glGetQueryObjecti64v(GLuint id, GLenum pname, int64_t* params){
-    if(!current_context || !current_context->timer_query)
+    if(!current_context)
         return;
+    // May be not needed, added just in case
+    if(!current_context->timer_query){
+        *params = 1;
+        return;
+    }
    es3_functions.glGetQueryObjecti64vEXT(id, pname, params);
 }
+
 void glGetQueryObjectui64v(GLuint id, GLenum pname, uint64_t* params){
-    if(!current_context || !current_context->timer_query)
+    if(!current_context)
         return;
+    if(!current_context->timer_query){
+        *params = 1;
+        return;
+    }
     es3_functions.glGetQueryObjectui64vEXT(id, pname, params);
 }
 void glQueryCounter(GLuint id, GLenum target){
