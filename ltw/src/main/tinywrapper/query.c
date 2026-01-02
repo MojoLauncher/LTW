474eea32554653515042dd37655fb9473a9f53f4
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2026-01-02 16:59:47 +0400

    Fix[query]: cleanup

--- ltw/src/main/tinywrapper/query.c
+++ ltw/src/main/tinywrapper/query.c
@@ -9,6 +9,7 @@
 
 #define CTX_CHECK() if (!current_context) return;
 
+// Minecraft uses these only for timer queries
 void glGetQueryObjecti64v(GLuint id, GLenum pname, int64_t* params){
     CTX_CHECK();
     // May be not needed, added just in case
@@ -33,37 +34,11 @@ void glQueryCounter(GLuint id, GLenum target){
     es3_functions.glQueryCounterEXT(id, target);
 }
 
+// Moved from main.c
 void glGetQueryObjectiv( 	GLuint id,
                             GLenum name,
                             GLint * params) {
     CTX_CHECK();
-    if(!current_context->timer_query && name == GL_QUERY_RESULT_AVAILABLE){
-        *params = 0;
-        return;
-    }
     // This is not recommended but i don't care
     es3_functions.glGetQueryObjectuiv(id, name, (GLuint*)params);
-}
-
-void glGetQueryObjectuiv(GLuint id, GLenum name, GLuint* params){
-    CTX_CHECK();
-    if(!current_context->timer_query && name == GL_QUERY_RESULT_AVAILABLE){
-        *params = 0;
-        return;
-    }
-    es3_functions.glGetQueryObjectuiv(id, name, params);
-}
-
-// Just in case
-void glBeginQuery(GLenum target, GLuint id){
-    CTX_CHECK();
-    if(!current_context->timer_query && target == GL_TIME_ELAPSED)
-        return;
-    es3_functions.glBeginQuery(target, id);
-}
-void glEndQuery(GLenum target){
-    CTX_CHECK();
-    if(!current_context->timer_query && target == GL_TIME_ELAPSED)
-        return;
-    es3_functions.glEndQuery(target);
 }
